75b04431f42f7d3df1e9f70f86a151b8a0fb5456
Senitdev <75537054+Senitdev@users.noreply.github.com>
2025-11-12 13:07:43 +0000

    add configuration
    Config Docker

--- db/database.go
+++ db/database.go
@@ -19,7 +19,7 @@ func Connect() {
 		log.Println("⚠️ Pas de fichier .env trouvé, on utilise les variables d'environnement du système")
 	}
 	dsn := fmt.Sprintf(
-		"host=%s user=%s  password=%s  dbname=%s port=%s sslmode=disable TimeZone=UTC",
+		"host=%s user=%s  password=%s  dbname=%s port=%s sslmode=require TimeZone=UTC",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
